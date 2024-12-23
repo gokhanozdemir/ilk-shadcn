5a0110ab5ba8a1abde3080791536b0f23d30fcec
Gökhan Özdemir <gn.ozdemir@gmail.com>
2024-12-23 13:11:07 +0300

    refactor App component to display button variants, sizes, and shapes in a structured layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,111 @@
-import { useState } from "react";
 import { Button } from "@/components/ui/Button";
+
+import {
+  ChevronRight,
+  ChevronDown,
+  ChevronLeft,
+  SlidersHorizontal,
+  MailOpen,
+  X,
+} from "lucide-react";
 
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const variants = [
+    "default",
+    "secondary",
+    "destructive",
+    "ghost",
+    "outline",
+    "link",
+  ];
+  const sizes = ["sm", "default", "lg", "icon"];
+  const shapes = ["rounded", "square", "pill"];
 
   return (
-    <>
-      <div className="container my-4 dashed">
-        <h1 className="text-3xl font-bold underline">Dodokod</h1>
-        <p>üye oldun mu? :)</p>
-        <div className="flex flex-wrap md:flex-row items-start gap-3 border-2 border-dashed border-primary p-4">
-          <Button>Button</Button>
-          <Button variant="secondary">Button</Button>
-          <Button variant="destructive">Button</Button>
-          <Button variant="ghost">Button</Button>
-          <Button variant="link">Button</Button>
-          <Button variant="outline">Button</Button>
-          <Button size="sm">Button</Button>
-          <Button>Button</Button>
-          <Button size="lg">Button</Button>
-          <Button shape="square">Button</Button>
-          <Button shape="pill">Button</Button>
-          <Button shape="pill" variant="outline" size="sm">
-            Button
-          </Button>
+    <div className="container my-4 flex flex-col gap-2">
+      <div className="grid grid-cols-3 md:grid-cols-1 gap-2 p-4 border-2 border-dashed border-secondary">
+        <h2 className="text-2xl text-primary font-bold col-span-3 md:col-span-1">
+          @dodokod 🦤
+        </h2>
+        <div className="flex flex-col md:flex-row items-start gap-3 border-2 border-dashed border-secondary p-4">
+          <h3 className="text-xl font-bold">Variants</h3>
+          {variants.map((variant) => {
+            return (
+              <Button key={variant} variant={variant}>
+                {variant}
+              </Button>
+            );
+          })}
+        </div>
+
+        <div className="flex flex-col md:flex-row items-start gap-3 border-2 border-dashed border-secondary p-4">
+          <h3 className="text-xl font-bold">Sizes</h3>
+          {sizes.map((size) => {
+            return (
+              <Button key={size} size={size}>
+                {size === "icon" ? <SlidersHorizontal /> : size}
+              </Button>
+            );
+          })}
+        </div>
+
+        <div className="flex flex-col md:flex-row items-start gap-3 border-2 border-dashed border-secondary p-4">
+          <h3 className="font-bold text-xl">Shapes</h3>
+          {shapes.map((shape) => {
+            return (
+              <Button key={shape} shape={shape}>
+                {shape}
+              </Button>
+            );
+          })}
         </div>
       </div>
-    </>
+      <div className="flex gap-3 flex-wrap p-4 border-2 border-dashed border-secondary">
+        <h3 className="font-bold text-xl w-full">Combinations</h3>
+        {shapes.map((shape) => {
+          return (
+            <div
+              className="flex-grow border-2 border-dashed p-1 border-secondary"
+              key={shape}
+            >
+              <h3 className="font-bold block w-full">{shape}</h3>
+              {sizes.map((size) => {
+                return (
+                  <div
+                    className="flex flex-col m-2 lg:flex-row lg:flex-wrap items-start gap-3 border-2 border-dashed border-secondary p-4"
+                    key={size}
+                  >
+                    <h4 className="text-sm font-bold w-full">size {size}</h4>
+                    {variants.map((variant) => {
+                      return (
+                        <div className="" key={variant}>
+                          <Button
+                            key={variant}
+                            size={size}
+                            shape={shape}
+                            variant={variant}
+                          >
+                            {size !== "icon" ? variant : ""}
+                            {variant === "default" && <ChevronRight />}
+                            {variant === "secondary" && <ChevronDown />}
+                            {variant === "destructive" && <X />}
+                            {variant === "ghost" && <ChevronLeft />}
+                            {variant === "outline" && <MailOpen />}
+                            {variant === "link" && <SlidersHorizontal />}
+                          </Button>
+                        </div>
+                      );
+                    })}
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
 
