02539aebae046f04d642b83aa689504d24dcbe98
Gökhan Özdemir <gn.ozdemir@gmail.com>
2024-12-23 12:25:46 +0300

    disable existing css and convert one button to tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,13 @@ function App() {
 
   return (
     <>
-      <div className="App dashed">
+      <div className="container my-4 dashed">
+        <h1 className="text-3xl font-bold underline">Dodokod</h1>
+        <p>üye oldun mu? :)</p>
+        <button className="bg-primary text-white text-center no-underline inline-block font-bold tracking-[0.2px] cursor-pointer min-w-fit px-10 py-4 rounded-md hover:bg-opacity-90">
+          Button
+        </button>
         <button className="sm">Button</button>
-        <button>Button</button>
         <button className="secondary">Button</button>
         <button className="secondary sm">Button</button>
         <button className="secondary lg pill">Button</button>
