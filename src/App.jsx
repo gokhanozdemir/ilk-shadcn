f27a030ce139700c77f4bb408218c85b8649579a
Gökhan Özdemir <gn.ozdemir@gmail.com>
2024-12-23 12:59:45 +0300

    add another shape variant

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Button } from "@/components/ui/Button";
+
 import "./App.css";
 
 function App() {
@@ -11,24 +11,22 @@ function App() {
       <div className="container my-4 dashed">
         <h1 className="text-3xl font-bold underline">Dodokod</h1>
         <p>üye oldun mu? :)</p>
-        <button className="bg-primary text-white text-center no-underline inline-block font-bold tracking-[0.2px] cursor-pointer min-w-fit px-10 py-4 rounded-md hover:bg-opacity-90">
-          Button
-        </button>
-        <button className="sm">Button</button>
-        <button className="secondary">Button</button>
-        <button className="secondary sm">Button</button>
-        <button className="secondary lg pill">Button</button>
-        <button className="ghost">Button</button>
-        <button className="destructive">Button</button>
-        <button className="link">Button</button>
-        <button className="lg">Button</button>
-        <button className="square">Button</button>
-        <button className="pill">Button</button>
-        <button className="pill lg">Button</button>
-        <button className="pill sm">Button</button>
-        <button className="outline sm">Button</button>
-        <button className="outline sm pill">Button</button>
-        <button className="outline lg square">Button</button>
+        <div className="flex flex-wrap md:flex-row items-start gap-3 border-2 border-dashed border-primary p-4">
+          <Button>Button</Button>
+          <Button variant="secondary">Button</Button>
+          <Button variant="destructive">Button</Button>
+          <Button variant="ghost">Button</Button>
+          <Button variant="link">Button</Button>
+          <Button variant="outline">Button</Button>
+          <Button size="sm">Button</Button>
+          <Button>Button</Button>
+          <Button size="lg">Button</Button>
+          <Button shape="square">Button</Button>
+          <Button shape="pill">Button</Button>
+          <Button shape="pill" variant="outline" size="sm">
+            Button
+          </Button>
+        </div>
       </div>
     </>
   );
